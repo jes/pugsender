b12c07b2c8cda84fad82c33554cc38ee320a0769
James Stanley <james@incoherency.co.uk>
2024-02-26 09:23:53 +0000

    Make toolbar overflow to next row when full

--- toolbar.go
+++ toolbar.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"image"
 
 	"gioui.org/layout"
@@ -17,58 +18,58 @@ type Toolbar struct {
 func (t Toolbar) Layout(gtx C, children ...layout.Widget) D {
 	size := 0
 	cs := gtx.Constraints
-	mainMax := cs.Max.X
-	crossMin, crossMax := cs.Min.Y, cs.Max.Y
-	remaining := mainMax
 	cgtx := gtx
 	childCall := make([]op.CallOp, len(children))
 	childDims := make([]D, len(children))
+
 	// Lay out Rigid children.
 	for i, child := range children {
 		macro := op.Record(gtx.Ops)
-		cgtx.Constraints = layout.Constraints{Min: image.Pt(0, crossMin), Max: image.Pt(remaining, crossMax)}
+		cgtx.Constraints = layout.Constraints{Min: image.Pt(0, cs.Min.Y), Max: image.Pt(cs.Max.X, cs.Max.Y)}
 		dims := t.Inset.Layout(cgtx, child)
 		c := macro.Stop()
 		sz := layout.Horizontal.Convert(dims.Size).X
 		size += sz
-		remaining -= sz
-		if remaining < 0 {
-			remaining = 0
-		}
 		childCall[i] = c
 		childDims[i] = dims
 	}
-	// fraction is the rounding error from a Flex weighting.
-	maxCross := crossMin
-	var maxBaseline int
+
+	maxHeight := cs.Min.Y
 	for i := range children {
-		if c := layout.Horizontal.Convert(childDims[i].Size).Y; c > maxCross {
-			maxCross = c
-		}
-		if b := childDims[i].Size.Y - childDims[i].Baseline; b > maxBaseline {
-			maxBaseline = b
+		if c := layout.Horizontal.Convert(childDims[i].Size).Y; c > maxHeight {
+			maxHeight = c
 		}
 	}
-	var mainSize int
+
+	xOffset, yOffset := 0, 0
+	totalWidth, totalHeight := 0, 0
 	for i := range children {
 		dims := childDims[i]
-		b := dims.Size.Y - dims.Baseline
-		var cross int
-		switch t.Alignment {
-		case layout.End:
-			cross = maxCross - layout.Horizontal.Convert(dims.Size).Y
-		case layout.Middle:
-			cross = (maxCross - layout.Horizontal.Convert(dims.Size).Y) / 2
-		case layout.Baseline:
-			cross = maxBaseline - b
+		width := layout.Horizontal.Convert(dims.Size).X
+		if xOffset+width > cs.Max.X {
+			fmt.Println(".")
+			xOffset = 0
+			// TODO: this makes each row equally tall, which is not necessarily the case
+			// (each row only needs enough Y height to contain the items on that row),
+			// but it's OK for now since we expect all buttons on a toolbar to have the
+			// same height
+			yOffset += maxHeight
 		}
-		pt := layout.Horizontal.Convert(image.Pt(mainSize, cross))
+		if xOffset+width > totalWidth {
+			totalWidth = xOffset + width
+		}
+		if yOffset+layout.Horizontal.Convert(dims.Size).Y > totalHeight {
+			totalHeight = yOffset + layout.Horizontal.Convert(dims.Size).Y
+		}
+
+		pt := layout.Horizontal.Convert(image.Pt(xOffset, yOffset))
 		trans := op.Offset(pt).Push(gtx.Ops)
 		childCall[i].Add(gtx.Ops)
 		trans.Pop()
-		mainSize += layout.Horizontal.Convert(dims.Size).X
+
+		xOffset += width
 	}
-	sz := layout.Horizontal.Convert(image.Pt(mainSize, maxCross))
-	sz = cs.Constrain(sz)
-	return D{Size: sz, Baseline: sz.Y - maxBaseline}
+
+	sz := layout.Horizontal.Convert(image.Pt(totalWidth, totalHeight))
+	return D{Size: cs.Constrain(sz)}
 }
